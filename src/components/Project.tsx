88296b4e92e82316497cb3ad0d13da2ba12cbe1f
Pabitra Patra <pabitrapatra369@gmail.com>
2023-10-30 19:08:50 +0500

    git run build err

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -16,7 +16,7 @@ const Project = (props: Props) => {
       <div className=" bg-violet-500 absolute top-0 left-0 h-full w-full flex justify-center items-center text-white p-4 text-xs text-center selection:rounded-lg opacity-0 group-hover:opacity-100 transition-all ease-in duration-300">
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries, but also the leap into electronic typesetting,
